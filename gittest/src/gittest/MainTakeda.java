7924e3323c888a3148b8060aaed580e7901d5e54
M-Takahara <M-Takahara@LAPTOP-EL6UH2VT>
2021-07-09 10:33:55 +0900

    高原に変更

--- gittest/src/gittest/MainTakeda.java
+++ gittest/src/gittest/MainTakeda.java
@@ -4,9 +4,7 @@ public class MainTakeda {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		for(;;) {
-		System.out.print("情シスモバイルアプリ専攻武田");
-	}
+		System.out.print("高原");
 	}
 
 }
