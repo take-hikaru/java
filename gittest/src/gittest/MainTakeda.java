832851812885131dba040fabb00526c5ef0f67d2
arita <arita@192.168.11.46>
2021-07-09 10:33:47 +0900

    名前追加

--- gittest/src/gittest/MainTakeda.java
+++ gittest/src/gittest/MainTakeda.java
@@ -5,7 +5,7 @@ public class MainTakeda {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		for(;;) {
-		System.out.print("情シスモバイルアプリ専攻武田");
+			System.out.print("有宗拓馬");
 	}
 	}
 
